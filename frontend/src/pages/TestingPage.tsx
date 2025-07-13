d9f7039b4fe259b82a3b1f0736deb1cdc0d28b56
Jati Sri Pamungkas <jatispamungkas357@gmail.com>
2025-07-13 16:27:10 +0700

    fix: testing backend api 3

--- frontend/src/pages/TestingPage.tsx
+++ frontend/src/pages/TestingPage.tsx
@@ -32,6 +32,13 @@ const TestingPage = () => {
 				>
 					Konten 4
 				</motion.div>
+				<motion.div
+					initial={{ opacity: 0, y: 50 }}
+					animate={{ opacity: 1, y: 0 }}
+					transition={{ delay: 1.0 }}
+				>
+					Konten 5
+				</motion.div>
 			</div>
 		);
 	};
